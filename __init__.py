eac7a685f606d4c8534f54bfc2e9a77247d046c5
Fraser Cumming <fraser.cumming32@gmail.com>
2026-07-17 15:21:45 +1000

    Delete __init__.py

--- __init__.py
+++
@@ -1,9 +0,0 @@
-# Copyright (C) 2026 Fraser Cumming
-# SPDX-License-Identifier: GPL-2.0-or-later
-"""QGIS entry point for CAD Importer."""
-
-
-def classFactory(iface):
-    from .plugin import CadImporterPlugin
-
-    return CadImporterPlugin(iface)
